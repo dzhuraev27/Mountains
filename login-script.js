2bb876012649778d5fc7ed96a768ef386c7d7a1d
JD <114643096+dzhuraev27@users.noreply.github.com>
2023-12-04 12:16:14 +0900

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 document.getElementById('login-form').addEventListener('submit', function (e) {
   e.preventDefault();
 
@@ -8,3 +15,15 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
   // Add your logic to handle the login functionality here
   console.log('Login submitted with data:', { email, password });
 });
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5iVhxrgr2fjmcRhYJXDy5UMw3_EqXK6I",
+  authDomain: "project1-90f73.firebaseapp.com",
+  databaseURL: "https://project1-90f73-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "project1-90f73",
+  storageBucket: "project1-90f73.appspot.com",
+  messagingSenderId: "469161293784",
+  appId: "1:469161293784:web:204b685195725683e224ad",
+  measurementId: "G-2J5GRG2843"
+};
